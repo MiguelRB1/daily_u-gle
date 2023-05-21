da6caeefc9fd46fa865c8c55d4756678bc437fb6
Mohamed <mohamedahmedsaleh50@gmail.com>
2023-05-21 09:12:27 -0700

    saving journals via the fetch api from front end

--- public/js/new.js
+++ public/js/new.js
@@ -7,12 +7,14 @@
       body: JSON.stringify({
        title: $('#jTitle').val(),
        description: $('#description').text(),
-       description: $('#description :selected').text(),
+       emoji: $('#emoji-select :selected').val(),
 
-       dateCreated: $('#datepicker').val(),
+    //    dateCreated: $('#datepicker').val(),
       }),
       headers: { 'Content-Type': 'application/json' },
-     });
+     }).then((res)=>{
+        console.log(res);
+     })
    
      // document.location.replace('/dashboard');
     };
