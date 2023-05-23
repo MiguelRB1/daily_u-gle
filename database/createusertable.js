8723d693d519a8002d11453c7e16581753d86e1b
Mohamed <mohamedahmedsaleh50@gmail.com>
2023-05-22 18:21:14 -0700

    added .env file and added the live running sql host instead of local host

--- database/createusertable.js
+++ database/createusertable.js
@@ -1,10 +1,35 @@
+// var mysql = require('mysql');
+
+// var con = mysql.createConnection({
+//   host: "http://localhost:5432",
+//   user: "root",
+//   password: "password",
+//   database: "Dailyugle"
+// });
+
+// con.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+//   var sql = "CREATE TABLE users (name VARCHAR(255), address VARCHAR(255))";
+//   con.query(sql, function (err, result) {
+//     if (err) throw err;
+//     console.log("Table created");
+//   });
+// });
+
+
+
+
+// // safe copy
+
+
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-  host: "http://localhost:5432",
-  user: "root",
-  password: "password",
-  database: "Dailyugle"
+  host: process.env.DB_HOST ,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DBNAME
 });
 
 con.connect(function(err) {
